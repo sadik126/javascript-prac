02b38542500fcea32b2f35d718f74abd9914022c
sadik126 <sadikhimel04@gmail.com>
2021-09-30 13:55:52 +0600

    changes

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 //console.log('hello console')
 //console.table({sadik:'this',marks:34})
-document.write("hello i am sadik");
+document.write("<i>hello</i> <br> <b>i am sadik</b><br>");
  
---
+++ variables.js
@@ -0,0 +1,7 @@
+var x = "sadik";
+var y = 90;
+
+x = "sad";
+
+document.write(x);
+document.write(y);
