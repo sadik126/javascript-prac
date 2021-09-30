7f2be3a82d4774493f2ea44e43f179bfbe06d554
sadik126 <sadikhimel04@gmail.com>
2021-09-30 13:25:00 +0600

    changes

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-console.log('hello console')
-console.table({sadik:'this',marks:34})
+//console.log('hello console')
+//console.table({sadik:'this',marks:34})
+document.write("hello i am sadik");
  
