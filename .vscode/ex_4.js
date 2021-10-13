3300291f01e932155b27e99d5641c3195820e60c
sadik126 <sadikhimel04@gmail.com>
2021-10-13 20:37:42 +0600

    cahnges

--- .vscode/ex_4.js
+++ .vscode/ex_4.js
@@ -12,6 +12,7 @@ constructor(booklist){
 //2. sadik = new library(['sadik','rohan','imran'])
 //3.for show booklist
 //sadik.getbooklist()
+//4.sadik.issuebook('sadik','himel')
 getbooklist(){
 this.booklist.forEach(element => {
    console.log(element) ;
@@ -19,7 +20,12 @@ this.booklist.forEach(element => {
 }
 
 issuebook(bookname,user){
+
+if(this.issuedbook[bookname] ==undefined){
 this.issuedbook[bookname] = user;
+}
+else
+console.log("this book is already issued");
 }
 
 returnbook(bookname){
